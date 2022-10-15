cb165e083aee2d5e203d10f9ffe5b5d2fc0caa19
hackticubrube <109615015+hackticubrube@users.noreply.github.com>
2022-10-15 12:02:14 +0300

    anubis
    anubis

---
+++ anubis/config.php
@@ -0,0 +1,14 @@
+<?php 
+
+$sname = "127.0.0.1";
+$uname = "anubis";
+$password = "@34fsedaanubis";
+$db_name = "anubisdb";
+
+
+define('SERVER' , $sname);
+define('DB', $db_name);
+define('USER', $uname);
+define('PASSWORD' , $password);
+define('cryptKey' , 'zanubis');
+define('namefolder', 'anubis');
